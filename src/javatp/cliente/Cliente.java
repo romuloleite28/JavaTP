0ef2ff96e21c398c4aa3f601d499fa963e8f3b91
Romulo Leite <Romulo Leite@DESKTOP-SPTD0GM.lan>
2021-07-07 18:14:40 +0100

    Update cliente

---
+++ src/javatp/cliente/Cliente.java
@@ -0,0 +1,80 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javatp.cliente;
+
+/**
+ *
+ * @author phrea
+ */
+public class Cliente {
+    
+    private int id;
+    private String dataNascimento;
+    private String genero;
+    private String patologias;
+    private int peso;
+    private int altura;
+
+    public Cliente(int id, String dataNascimento, String genero, String patologias, int peso, int altura) {
+        this.id = id;
+        this.dataNascimento = dataNascimento;
+        this.genero = genero;
+        this.patologias = patologias;
+        this.peso = peso;
+        this.altura = altura;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public String getPatologias() {
+        return patologias;
+    }
+
+    public int getPeso() {
+        return peso;
+    }
+
+    public int getAltura() {
+        return altura;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setDataNascimento(String dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    public void setPatologias(String patologias) {
+        this.patologias = patologias;
+    }
+
+    public void setPeso(int peso) {
+        this.peso = peso;
+    }
+
+    public void setAltura(int altura) {
+        this.altura = altura;
+    }
+    
+    
+    
+}
